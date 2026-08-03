89fe2d49cedd58c90954b4a9bc5e1872e73b5c7c
Toavina <hei.toavina.2@gmail.com>
2026-08-03 07:48:45 +0300

    Feat/crud user
    * feat:add the new crud for user
    
    * style:applicate the format
    
    * feat(UserService):add the new spec to find with email

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,7 +1,11 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.User;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, UUID> {}
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+  Optional<User> findByEmail(String email);
+}
