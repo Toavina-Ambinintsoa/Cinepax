256f593b9fadc904509318ed0514618d46ec71e1
Tokiniaina RAKOTOZANANY <hei.tokiniaina.2@gmail.com>
2026-08-06 11:06:30 +0300

    feat: add controllers

---
+++ src/main/java/com/example/demo/endpoint/rest/controller/MovieController.java
@@ -0,0 +1,24 @@
+package com.example.demo.endpoint.rest.controller;
+
+import com.example.demo.entity.Movie;
+import com.example.demo.service.MovieService;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("movies")
+public class MovieController {
+  private final MovieService movieService;
+
+  @Autowired
+  public MovieController(MovieService movieService) {
+    this.movieService = movieService;
+  }
+
+  @PutMapping("{id}")
+  public ResponseEntity<Movie> updateMovie(@PathVariable UUID id, @RequestBody Movie movie) {
+    return ResponseEntity.status(200).body(movieService.update(id, movie));
+  }
+}
