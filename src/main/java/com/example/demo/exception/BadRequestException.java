2714040693cc70c1d3d77c5add7c1a5ba84b1b70
Tokiniaina RAKOTOZANANY <hei.tokiniaina.2@gmail.com>
2026-08-06 21:02:42 +0300

    some random change to force redeploy

--- src/main/java/com/example/demo/exception/BadRequestException.java
+++ src/main/java/com/example/demo/exception/BadRequestException.java
@@ -2,7 +2,7 @@ package com.example.demo.exception;
 
 /**
  * A mapper vers un 400 côté controller. Utilisée pour les règles métier (ex: siège déjà réservé,
- * salle pleine...).
+ * salle pleine...)
  */
 public class BadRequestException extends RuntimeException {
 
