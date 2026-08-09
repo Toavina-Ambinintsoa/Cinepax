f3961eabde5fab4213e148cb022e63538af8be66
Tokiniaina RAKOTOZANANY <hei.tokiniaina.2@gmail.com>
2026-08-09 12:14:26 +0300

    fix: add forgotten annotation in repository

--- src/main/java/com/example/demo/repository/RoomRepository.java
+++ src/main/java/com/example/demo/repository/RoomRepository.java
@@ -3,5 +3,7 @@ package com.example.demo.repository;
 import com.example.demo.entity.Room;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoomRepository extends JpaRepository<Room, UUID> {}
